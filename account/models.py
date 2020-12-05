819d94193592908fd9482657bb9522ac58528091
turqcacke <60967566+turqcacke@users.noreply.github.com>
2020-12-05 16:46:15 +0500

    Upd: event/models fixed

--- account/models.py
+++ account/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
     avatar = models.ImageField(upload_to='avatars/%Y/%m/%d', blank=True)
-    contact_number = models.IntegerField(max_length=32)
+    contact_number = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
