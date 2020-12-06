b7118f7f406c8a4c15ba191426dc6a22ca3e7108
turqcacke <60967566+turqcacke@users.noreply.github.com>
2020-12-06 11:00:26 +0500

    Feature: saving qr_codes become more safe

--- event/views.py
+++ event/views.py
@@ -8,28 +8,30 @@ from . import models
 import qrcode
 import os
 
-TEMP_PATH = settings.MEDIA_ROOT + 'temp/'
-_QR_CODE_EXT = 'png'
-QR_CODE_SAVE_NAME = '{user_id}_{event_id}.' + _QR_CODE_EXT
-
 
 def generate_qr_code(data):
     qr_code = qrcode.make(data)
-    qr_code_path = TEMP_PATH + 'temp'
+    qr_code_path = settings.TEMP_PATH + 'temp'
     qr_code.save(qr_code_path)
     return qr_code_path
 
 
 def add_qr_code_to_participant(data, participant, user, event):
     old_path = generate_qr_code(data)
-    participant.qr_code.save(QR_CODE_SAVE_NAME.format(user_id=user.pk, event_id=event.id), open(old_path, "rb"), True)
+    new_path = settings.QR_CODE_SAVE_NAME.format(namespace=user.username, user_id=user.pk,
+                                                 event_id=event.id, ext=settings.QR_CODE_EXT)
+    participant.qr_code.save(new_path, open(old_path, "rb"), True)
     os.remove(old_path)
 
 
 @login_required
-def add_participant(request, pk):
+def add_participant(request, slug, year, month, day):
     if request.method == 'POST':
-        event = get_object_or_404(models.Event, pk=pk)
+        event = get_object_or_404(models.Event,
+                                  slug=slug,
+                                  year=year,
+                                  month=month,
+                                  day=day)
         try:
             participant = event.participants.get(user=request.user)
         except models.Participant.DoesNotExist:
@@ -46,7 +48,7 @@ def add_participant(request, pk):
 
 @login_required
 def events_list(request):
-    if 'display_my' in request.GET == 'participating':
+    if 'display_my' in request.GET and request.GET['display_my'] == 'participating':
         list_events = models.Event.objects.filter(owner=request.user)
     elif 'display_my' in request.GET and request.GET['display_my'] == 'created':
         list_events = models.Event.objects.filter(participants__user=request.user)
@@ -69,10 +71,10 @@ def events_list(request):
 @login_required
 def list_participants(request, slug, year, month, day):
     participants = get_object_or_404(models.Event,
-                                     slug=slug,
-                                     year=year,
-                                     month=month,
-                                     day=day).participants.all()
+                                     created__slug=slug,
+                                     created__year=year,
+                                     created__month=month,
+                                     created__day=day).participants.all()
     return render(request,
                   '',
                   {'participants': participants})
@@ -81,10 +83,10 @@ def list_participants(request, slug, year, month, day):
 @login_required
 def event_detail(request, slug, year, month, day):
     event = get_object_or_404(models.Event,
-                              slug=slug,
-                              year=year,
-                              month=month,
-                              day=day)
+                              created__slug=slug,
+                              created__year=year,
+                              created__month=month,
+                              created__day=day)
     return render(request,
                   '',
                   {'even': event})
