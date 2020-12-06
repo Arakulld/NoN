b804e7bfcc1d46705b27807238dbb3368a9e90bc
turqcacke <60967566+turqcacke@users.noreply.github.com>
2020-12-06 05:08:50 +0500

    Add: models, logic in views.py

--- event/models.py
+++ event/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+import os
 
 
 class Event(models.Model):
-    slug = models.SlugField(max_length=255, unique_for_date='')
+    slug = models.SlugField(max_length=255, unique_for_date='created')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL,
                               related_name='events_created',
                               on_delete=models.CASCADE)
+    company = models.CharField(max_length=64, blank=True, null=True)
 
     title = models.CharField(max_length=32, blank=True)
     price = models.IntegerField(default=0)
     description = models.TextField(blank=True)
     start_time = models.DateTimeField(blank=True)
     end_time = models.DateTimeField(blank=True)
+    longitude = models.CharField(max_length=32, blank=True, null=True)
+    latitude = models.CharField(max_length=32, blank=True, null=True)
 
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('-created',)
@@ -32,11 +35,16 @@ class Participant(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              related_name='participating_events',
                              on_delete=models.CASCADE, )
-    attended = models.BooleanField(default=False)
-    qr_code = models.ImageField(upload_to='participants/qr_codes/%Y/%m/%d')
+    qr_code = models.ImageField(upload_to='participants/qr_codes/',
+                                blank=True)
 
     def __str__(self):
-        return str(self.event)
+        return str(self.event) + '|' + str(self.user)
+
+    def delete(self, *args, **kwargs):
+        if self.qr_code:
+            os.remove(self.qr_code.path)
+        super(Participant, self).delete(*args, **kwargs)
 
 
 class News(models.Model):
@@ -68,3 +76,27 @@ class Attendance(models.Model):
 
     def __str__(self):
         return str(self.participant) + str(self.date)
+
+
+class Question(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                related_name='created_themes')
+    related_event = models.ForeignKey(Event,
+                                      on_delete=models.CASCADE,
+                                      related_name='questions')
+    slug = models.CharField(max_length=64)
+    editable = models.BooleanField(default=False)
+    title = models.CharField(max_length=64)
+    message = models.TextField()
+
+
+class Comment(models.Model):
+    related_question = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                         on_delete=models.CASCADE,
+                                         related_name='question_comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='author_comments')
+    message = models.TextField()
+    created = models.DateTimeField(auto_now=True)
--- event/views.py
+++ event/views.py
@@ -1,3 +1,90 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render, get_object_or_404, redirect
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.contrib import messages
+from . import models
+import qrcode
+import os
 
-# Create your views here.
+TEMP_PATH = settings.MEDIA_ROOT + 'temp/'
+_QR_CODE_EXT = 'png'
+QR_CODE_SAVE_NAME = '{user_id}_{event_id}.' + _QR_CODE_EXT
+
+
+def generate_qr_code(data):
+    qr_code = qrcode.make(data)
+    qr_code_path = TEMP_PATH + 'temp'
+    qr_code.save(qr_code_path)
+    return qr_code_path
+
+
+def add_qr_code_to_participant(data, participant, user, event):
+    old_path = generate_qr_code(data)
+    participant.qr_code.save(QR_CODE_SAVE_NAME.format(user_id=user.pk, event_id=event.id), open(old_path, "rb"), True)
+    os.remove(old_path)
+
+
+@login_required
+def add_participant(request, pk):
+    if request.method == 'POST':
+        event = get_object_or_404(models.Event, pk=pk)
+        try:
+            participant = event.participants.get(user=request.user)
+        except models.Participant.DoesNotExist:
+            raise Http404
+        add_qr_code_to_participant('',
+                                   participant=participant,
+                                   event=event,
+                                   user=request.user)
+        messages.success(request, 'Registered successfully.')
+        redirect('')
+    else:
+        raise Http404
+
+
+@login_required
+def events_list(request):
+    if 'display_my' in request.GET == 'participating':
+        list_events = models.Event.objects.filter(owner=request.user)
+    elif 'display_my' in request.GET and request.GET['display_my'] == 'created':
+        list_events = models.Event.objects.filter(participants__user=request.user)
+    else:
+        list_events = models.Event.objects.all()
+    paginator = Paginator(list_events, 20)
+    page = request.GET.get('page')
+    try:
+        events = paginator.page(page)
+    except PageNotAnInteger:
+        events = paginator.page(1)
+    except EmptyPage:
+        events = paginator.page(paginator.num_pages)
+    return render(request,
+                  '',
+                  {'page': page,
+                   'events': events})
+
+
+@login_required
+def list_participants(request, slug, year, month, day):
+    participants = get_object_or_404(models.Event,
+                                     slug=slug,
+                                     year=year,
+                                     month=month,
+                                     day=day).participants.all()
+    return render(request,
+                  '',
+                  {'participants': participants})
+
+
+@login_required
+def event_detail(request, slug, year, month, day):
+    event = get_object_or_404(models.Event,
+                              slug=slug,
+                              year=year,
+                              month=month,
+                              day=day)
+    return render(request,
+                  '',
+                  {'even': event})
